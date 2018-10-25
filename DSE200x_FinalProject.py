18897bf5c0115ae37137df9daca82d449a4914d5
sarahrustan <40900760+sarahrustan@users.noreply.github.com>
2018-10-25 15:46:53 -0400

    Add files via upload

--- DSE200x_FinalProject.py
+++ DSE200x_FinalProject.py
@@ -8,11 +8,16 @@
 
 # # Final Project: Exploring attitudes towards opioids using k-modes clustering algorithm
 
-# ### Objective: To identify clusters of demographic and experiential variables that are associated with attitudes towards opioids.
-# 
-# ### Methods: Combine data from mutiple sources to create a variable identifying each survey response geography as urban, suburban, or rural. Conduct cluster analysis using k-modes clustering algorithm (an unsupervised machine learning algorithm for categorical variables) to determine whether there are clusters of demographic and experiential variables associated with attitudes towards opioids, including urban, suburban or rural geography.
+# ### Objective
 
-# Datasources:
+# To identify clusters of demographic and experiential variables that are associated with attitudes towards opioids.
+
+# ### Method
+
+# Combine data from mutiple sources to create a variable identifying each survey response geography as urban, suburban, or rural. Conduct cluster analysis using k-modes clustering algorithm (an unsupervised machine learning algorithm for categorical variables) to determine whether there are clusters of demographic and experiential variables associated with attitudes towards opioids, including urban, suburban or rural geography.
+
+# ### Datasources
+
 # 1. Proprietary survey, n = 1,200
 #     
 #     - Geographic: _VGeoRegion_ (String): State; _Vpostal_ (String): Zip code
@@ -29,9 +34,20 @@
 # 3. B25001 HOUSING UNITS by ZCTA, 2012-2016 American Community Survey 5-Year Estimates, n = 33,120 https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=ACS_16_5YR_B25001&prodType=table
 # 
 #     - _GEO.id2_: Five digit ZIP Code Tabulation Area Census Code; *HD01_VD01*: Housing Units, Estimated
+
+# ### Results
+
+# Three distinct groups of survey respondents were identified. The first two groups embody attitudes typical of plaintiff- and defense-oriented jurors in similar cases, while the third group appears to have demonstrated a significant acquiescence bias, which is a tendency to either answer 'Yes' or choose the first response option given. 
+
+# As shown below, the graph representing the costs associated with different number of clusters indicates that the 'elbow' occurs at three clusters; in other words, the amount that model fit improves with the addition of each additional cluster decreases after three clusters. This suggests that three clusters is the ideal number for this analysis. However, since identifying differences in attitudes between sub-groups of plaintiff- or defense-oriented jurors  could prove useful in framing the case, future research could include using the k-modes clustering algorithm for 4 (and possibly more) clusters.
+# 
+# 
 # 
 
-# ### Identify each Zip Code Tabulation Area as Urban, Suburban, or Rural based on housing density
+# 
+# ### Preprocessing and analysis
+
+# #### Identify each Zip Code Tabulation Area as Urban, Suburban, or Rural based on housing density
 
 # ZCTA housing density = Number of housing units in ZCTA/Square miles in ZCTA
 
@@ -787,12 +803,6 @@ P2
 # In[122]:
 
 
-# Plot
+# Plot clusters
 parallel_plot(P2)
 
-
-# # Results
-
-# Three distinct groups of survey respondents were identified. The first two groups embody attitudes typical of plaintiff- and defense-oriented jurors in similar cases, while the third group appears to have demonstrated a significant acquiescence bias, which is a tendency to either answer 'Yes' or choose the first response option given. 
-
-# As above, the graph representing the costs associated with different number of clusters indicates that the 'elbow' occurs at three clusters; in other words, the amount that model fit improves with the addition of each additional cluster decreases after three clusters. This suggests that three clusters is the ideal number for this analysis. However, since identifying differences in attitudes between sub-groups of plaintiff- or defense-oriented jurors  could prove useful in framing the case, future research could include using the k-modes clustering algorithm for 4 (and possibly more) clusters.
